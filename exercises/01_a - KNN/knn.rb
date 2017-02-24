0431df1536788b0eae6dcf47cedabd532dcc8608
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2017-02-24 07:30:04 +0100

    Create condensed set

--- exercises/01_a - KNN/knn.rb
+++ exercises/01_a - KNN/knn.rb
@@ -11,18 +11,44 @@ def read_classifications(filepath)
   end
 end
 
-def distance(v1, v2, type: :euclidiean)
-  sum = 0
-
-  v2.each_with_index do |v, index|
-    sum += ((v - v1[index]) ** 2)
+def b(switch = true)
+  t = Benchmark.realtime do
+    yield
   end
+  puts t
+end
 
+def euclidiean_distance(v1, v2)
+  sum = 0
+  for i in 0..783
+    sum += ((v2[i] - v1[i]) ** 2)
+  end
   Math.sqrt(sum)
 end
 
-def knn(k: 1, training_set:, sample:)
-  training_set.min_by(k.to_i) { |c| distance(c.vector, sample.vector) }
+def manhattan_distance(v1, v2)
+  sum = 0
+  for i in 0..783
+    sum += (v1[i] - v2[i]).abs
+  end
+  sum
+end
+
+def distance(v1, v2, metric: :euclidiean)
+  case metric.to_sym
+  when :euclidiean
+    euclidiean_distance(v1, v2)
+  when :manhattan
+    manhattan_distance(v1, v2)
+  else
+    euclidiean_distance(v1, v2)
+  end
+end
+
+def knn(k: 1, training_set:, sample:, distance_metric: :euclidiean)
+  training_set.min_by(k.to_i) do |c|
+    distance(c.vector, sample.vector, metric: distance_metric)
+  end
 end
 
 def open_as_image(vector)
@@ -43,9 +69,9 @@ def condense(training_set)
   changes = true
   condensed = [training_set.shift]
   while changes do
+    print '*'
     changes = false
     training_set.each_with_index do |c, index|
-      puts index
       nearest = knn(training_set: condensed, sample: c, k: 1).first
       unless nearest.label == c.label
         condensed << training_set.delete(c)
@@ -63,22 +89,33 @@ ensure
   puts 'Successfully written condensed training set to train_condensed.csv.'
 end
 
-k = ARGV.first || 1
-puts "k = #{k}"
+k = ARGV[0] || 1
+distance_metric = ARGV[1] || :euclidiean
+puts "k = #{k}, metric = #{distance_metric}"
 
 puts "Loading data sets"
-training_set = read_classifications('train.csv')
+training_set = read_classifications('train_condensed.csv')
+# training_set = read_classifications('train.csv')
 test_set = read_classifications('test.csv')
 
-t = Benchmark.realtime do
-  condense(training_set)
-end
+# condense(training_set)
 
-puts "Took #{t} seconds"
-
-# puts "Classifying data"
-# test_set.each do |sample|
-#   k_nearest = knn(training_set: training_set, sample: sample, k: k)
-#   nearest = k_nearest.max_by { |c| k_nearest.count(c) }
-#   puts "Classified #{sample.label} as #{nearest.label}"
+# t = Benchmark.realtime do
+#   condense(training_set)
 # end
+# puts "Took #{t} seconds"
+
+puts "Classifying data"
+
+test_set.each do |sample|
+  b do
+    k_nearest = knn(
+      training_set: training_set,
+      sample: sample,
+      k: k,
+      distance_metric: distance_metric
+    )
+    nearest = k_nearest.max_by { |c| k_nearest.count(c) }
+    puts "Classified #{sample.label} as #{nearest.label}"
+  end
+end
