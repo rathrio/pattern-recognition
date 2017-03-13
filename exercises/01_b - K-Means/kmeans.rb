8887f3cf77e2c8c7e59f032a650ed759a5b873d0
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2017-03-13 20:25:39 +0100

    Add kmeans implementation

--- exercises/01_b - K-Means/kmeans.rb
+++ exercises/01_b - K-Means/kmeans.rb
@@ -1,5 +1,13 @@
 #!/usr/bin/env ruby
 
+def distance(v1, v2)
+  sum = 0
+  for i in 0..783
+    sum += ((v2[i] - v1[i]) ** 2)
+  end
+  Math.sqrt(sum)
+end
+
 # Some extensions for colored terminal output.
 class String
   def colorize(color_code)
@@ -34,22 +42,68 @@ def b
   puts t
 end
 
-# Helper to render vector as png and open it with a Mac's default application.
-def open_as_image(vector)
-  require 'chunky_png'
-  png = ChunkyPNG::Image.new(28, 28)
+class Cluster
+  attr_reader :center, :classifications
 
-  vector.each_slice(28).with_index do |row, i|
-    row.each_with_index do |pixel_value, j|
-      png[j, i] = ChunkyPNG::Color.grayscale(pixel_value)
-    end
+  def initialize(center, classifications)
+    @center = center
+    @classifications = classifications
   end
 
-  png.save('/tmp/foo.png', interlaced: true)
-  system "open /tmp/foo.png"
+  def recompute_center
+    new_center = Array.new(784, 0)
+
+    classifications.each do |c|
+      for i in 0..783
+        new_center[i] += c.vector[i]
+      end
+    end
+
+    for i in 0..783
+      new_center[i] = new_center[i] / classifications.count
+    end
+
+    @center = center
+  end
+
+  def add(classification)
+    @classifications << classification
+  end
 end
+
+def kmeans(ks: [5, 7, 9, 10, 12, 15], training_set:, iterations: 1)
+  ks.each do |k|
+    # Choose K initial cluster centers
+    centers = training_set.sample(k)
+    clusters = centers.map { |c| Cluster.new(c.vector, []) }
+
+    iterations.times do
+      training_set.each do |c|
+        nearest_cluster = clusters.min_by { |cluster| distance(cluster.center, c.vector) }
+        nearest_cluster.add(c)
+        print '.'.green
+      end
+
+      puts "\nRecomputing centers..."
+      clusters.each(&:recompute_center)
+    end
+
+    puts "\nStats for k=#{5}"
+    clusters.each_with_index do |cluster, index|
+      puts "\nCluster #{index}\n----------"
+      all_labels = cluster.classifications.map(&:label)
+      (0..9).each do |l|
+        puts "#{l}: #{all_labels.count(l)}"
+      end
+    end
+  end
+end
+
 
 if ARGV.first =~ /-h|--help/
   puts "Usage: ./#{File.basename(__FILE__)}"
   exit
 end
+
+training_set = read_classifications('training_set.csv')
+b { kmeans(ks: [15], training_set: training_set, iterations: 5) }
