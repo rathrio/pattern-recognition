100480ad4bb7496c05d267bb33c54703dd20300e
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2017-03-14 16:14:43 +0100

    Some refactoring

--- exercises/01_b - K-Means/kmeans.rb
+++ exercises/01_b - K-Means/kmeans.rb
@@ -21,12 +21,12 @@ end
 
 # Benchmark helper. Will print seconds it took to run passed block.
 # e.g. b { sleep(2) } # => Will print "2.0000..."
-def b
+def b(msg = nil)
   require 'benchmark'
   t = Benchmark.realtime do
     yield
   end
-  puts t
+  puts "\n#{msg}: #{t.round(4)}".strip
 end
 
 class Cluster
@@ -47,31 +47,45 @@ class Cluster
   def add(classification)
     @classifications << classification
   end
+
+  def labels
+    classifications.map(&:label)
+  end
 end
 
-def kmeans(ks: [5, 7, 9, 10, 12, 15], training_set:, iterations: 1)
-  ks.each do |k|
-    # Choose K initial cluster centers
-    centers = training_set.sample(k)
-    clusters = centers.map { |c| Cluster.new(c.vector, []) }
+def kmeans(k:, training_set:, iterations:)
+  # Choose K initial cluster centers
+  centers = training_set.sample(k)
+  clusters = centers.map { |c| Cluster.new(c.vector, []) }
 
-    iterations.times do |i|
-      training_set.each do |c|
-        nearest_cluster = clusters.min_by do |cluster|
-          distance(cluster.center, c.vector)
-        end
-        nearest_cluster.add(c)
-        print '.'
+  iterations.times do |i|
+    training_set.each do |c|
+      nearest_cluster = clusters.min_by do |cluster|
+        distance(cluster.center, c.vector)
       end
-
-      puts "\nRecomputing centers..."
-      clusters.each(&:recompute_center) unless (i + 1) == iterations
+      nearest_cluster.add(c)
+      print '.'
     end
+
+    # Don't recompute centers in last iteration because we're done reassigning
+    # vectors.
+    unless (i + 1) == iterations
+      puts "\nRecomputing centers"
+      clusters.each(&:recompute_center)
+    end
+  end
+
+  clusters
+end
+
+def cluster(ks: [5, 7, 9, 10, 12, 15], training_set:, iterations: 1)
+  ks.each do |k|
+    clusters = kmeans(k: k, training_set: training_set, iterations: iterations)
 
     puts "\nStats for k=#{k}"
     clusters.each_with_index do |cluster, index|
       puts "\nCluster #{index}\n----------"
-      all_labels = cluster.classifications.map(&:label)
+      all_labels = cluster.labels
       (0..9).each do |l|
         puts "#{l}: #{all_labels.count(l)}"
       end
@@ -85,5 +99,6 @@ if ARGV.first =~ /-h|--help/
   exit
 end
 
-training_set = read_classifications('training_set.csv')
-b { kmeans(ks: [9], training_set: training_set, iterations: 100) }
+training_set = []
+b('Loading training set') { training_set = read_classifications('training_set.csv') }
+b('Clustering') { cluster(ks: [9], training_set: training_set, iterations: 100) }
